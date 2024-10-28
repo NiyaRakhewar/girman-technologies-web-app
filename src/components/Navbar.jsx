b1a29401e8649a968cbaf7b8a9e6561b0309e3da
Niharika Rakhewar <111850896+NiyaRakhewar@users.noreply.github.com>
2024-10-29 03:09:51 +0530

    add navbar, setup Shadcn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import company_logo from './../assets/company_logo.png';
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex items-center justify-around bg-#FFFFFF p-4 shadow-md">
+      <div onClick={() => navigate("/")} className="cursor-pointer">
+        <img src={company_logo} alt="Girman Technologies" className="h-10 w-auto" />
+      </div>
+
+      <div className="flex space-x-4 text-black">
+        <Link to="/" className="hover:text-gray-300">Search</Link>
+        <Link to="https://girmantech.com" target="_blank" className="hover:text-gray-300">Website</Link>
+        <Link to="https://www.linkedin.com/company/girmantech/" target="_blank" className="hover:text-gray-300">LinkedIn</Link>
+        <a href="mailto:girish@girmantech.com" className="hover:text-gray-300">Contact</a>
+      </div>
+    </nav>
+  );
+};
